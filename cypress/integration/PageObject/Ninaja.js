6dea4a1bf0527221b91b0ca63f3ee50b64e39dd5
itachi019 <salunkhe19rohan@gmail.com>
2024-07-27 18:21:46 +0530

    new test cases

--- cypress/integration/PageObject/Ninaja.js
+++ cypress/integration/PageObject/Ninaja.js
@@ -13,16 +13,134 @@ describe('TutorialsNinja Checkout', () => {
 
 
 
-    it('checkout process', () => {
+    it('checkout process1', () => {
+        //Test case for out of stock products
         register.EnterURL()
         register.Register()
         homePage.EnterURL()
         homePage.search('iphone')
+        cy.get('div.image').click();
         product.addToCart()
         checkout.checkoutpro()
+        cy.contains('Products marked with *** are not available in the desired quantity or not in stock!')
+        cy.window().then(win => {
+            win.close()
+        })
+    });
+
+    it('checkout process2', () => {
+        //Test case for checkout with out of stock products
+        register.EnterURL()
+        register.Register()
+        homePage.EnterURL()
+        homePage.search('mac')
+        cy.get('img[title="iMac"]').should('be.visible').click();
+        product.addToCart()
+        checkout.checkoutpro()
+        cy.contains('Products marked with *** are not available in the desired quantity or not in stock!')
+        cy.window().then(win => {
+            win.close()
+        })
+    });
+
+    it('checkout process3', () => {
+        //Test case for out of stock products
+        register.EnterURL()
+        register.Register()
+        homePage.EnterURL()
+        homePage.search('mac')
+        cy.get('img[title="MacBook"]').click();
+        product.addToCart()
+        checkout.checkoutpro()
+        cy.contains('Products marked with *** are not available in the desired quantity or not in stock!')
+        cy.window().then(win => {
+            win.close()
+        })
+    });
+
+    it('checkout process4', () => {
+        //Test case for out of stock products
+        register.EnterURL()
+        register.Register()
+        homePage.EnterURL()
+        homePage.search('mac')
+        cy.get('img[alt="MacBook Air"]').click();
+        product.addToCart()
+        checkout.checkoutpro()
+        cy.contains('Products marked with *** are not available in the desired quantity or not in stock!')
+        cy.window().then(win => {
+            win.close()
+        })
+    });
+
+    it('search function1', () => {
+        //test case for search not match
+        register.EnterURL()
+        register.Register()
+        homePage.EnterURL()
+        homePage.search('camera')
+        cy.contains('There is no product that matches the search criteria.').should('be.visible');
         cy.window().then(win => {
             win.close()
         })
     });
     
+    it('search function2', () => {
+        //test case for empty search entered
+        register.EnterURL()
+        register.Register()
+        homePage.EnterURL()
+        homePage.search('000')
+        cy.contains('There is no product that matches the search criteria.').should('be.visible');
+        cy.window().then(win => {
+            win.close()
+        })
+        });
+
+        it('checkout process5', () => {
+            //Test case for out of stock products
+            register.EnterURL()
+            register.Register()
+            homePage.EnterURL()
+            homePage.search('   ')
+            cy.get('img[title="HTC Touch HD"]').click();
+            product.addToCart()
+            checkout.checkoutpro()
+            cy.contains('Products marked with *** are not available in the desired quantity or not in stock!')
+            cy.window().then(win => {
+                win.close()
+            })
+        });
+
+        it('checkout process5', () => {
+            //Test case for out of stock products
+            register.EnterURL()
+            register.Register()
+            homePage.EnterURL()
+            homePage.search('   ')
+            cy.get('img[title="HTC Touch HD"]').click();
+            product.addToCart()
+            checkout.checkoutpro()
+            cy.contains('Products marked with *** are not available in the desired quantity or not in stock!')
+            cy.window().then(win => {
+                win.close()
+            })
+        });
+
+        it.only('checkout process6', () => {
+            //Test case for out of stock products
+            register.EnterURL()
+            register.Register()
+            homePage.EnterURL()
+            homePage.search('   ')
+            cy.get('img[title="iPod Touch"]').click();
+            product.addToCart()
+            checkout.checkoutpro()
+            cy.contains('Products marked with *** are not available in the desired quantity or not in stock!')
+            cy.window().then(win => {
+                win.close()
+            })
+        });
+
+       
 });
--- cypress/support/autoproject/TutorialsNinja/productPage.js
+++ cypress/support/autoproject/TutorialsNinja/productPage.js
@@ -1,6 +1,7 @@
 class Product{
    addToCart(){
-    cy.xpath('//div[@class="button-group"]/button[1]').click();
+  //  cy.xpath('//div[@class="button-group"]/button[1]').click();
+  cy.get('#button-cart').should('be.visible').click();
     cy.xpath('//a[contains(text(), "shopping cart")]').click();
    }
 }
